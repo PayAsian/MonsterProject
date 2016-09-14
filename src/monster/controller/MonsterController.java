783c1b96467a70f0f54cf82c30c9ef46bdf3e69d
PayAsian <ptre8426@csddocs.org>
2016-09-14 12:58:11 -0600

    My monster now exists in java

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,5 +1,30 @@
 package monster.controller;
 
-public class MonsterController {
+import monster.model.MarshmallowMonster;
+
+public class MonsterController 
+{
+	private MarshmallowMonster firstMonster;
+	
+	public MonsterController()
+	{
+		firstMonster = new MarshmallowMonster("Fluffy", 2, 4, 0, 2, false);
+	}
+
+	public void start()
+	{
+		System.out.println("We made monsters today!");
+		System.out.println("here is mine " + firstMonster);
+	}
+	/* monster name is Fluffy
+	 * has 2 antenna
+	 * has 4 legs
+	 * has 0 nose
+	 * has 2 eyes
+	 * does not have a belly button 
+	 */
+
+
+
 
 }
--- src/monster/controller/MonsterRunner.java
+++ src/monster/controller/MonsterRunner.java
@@ -1,5 +1,18 @@
 package monster.controller;
 
-public class MonsterRunner {
+public class MonsterRunner 
+{
+	public static void main (String [] args)
+	{
+		MonsterController monsterAppController = new MonsterController();
+		monsterAppController.start();
+	}
+
+
+
+
+
+
+
 
 }
