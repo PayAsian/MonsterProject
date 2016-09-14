aa295561f9ac8bd7426c77f28247ae0cf549e1ea
PayAsian <ptre8426@csddocs.org>
2016-09-14 12:45:57 -0600

    added body parts and made a physical bode for FLUFFY

---
+++ src/monster/model/MarshmallowMonster.java
@@ -0,0 +1,49 @@
+package monster.model;
+
+public class MarshmallowMonster
+{
+	private String name;
+	private double antennaCount;
+	private int eyeCount;
+	private int legCount;
+	private int noseCount;
+	private boolean hasBellyButton;
+	
+	public MarshmallowMonster()
+	{
+		this.name = "MonsterName";
+		this.antennaCount = -123456.32;
+		this.eyeCount = -65435;
+		this.legCount = -98768415;
+		this.noseCount = -2;
+		this.hasBellyButton = false;
+	}
+	
+	public MarshmallowMonster(String name, double antennaCount, int eyeCount, int legCount, int noseCount, boolean hasBellyButton)
+	{
+		this.name = name;
+		this.antennaCount = antennaCount;
+		this.eyeCount = eyeCount;
+		this.legCount = legCount;
+		this.noseCount = noseCount;
+		this.hasBellyButton = hasBellyButton;
+		
+	}
+
+	/* monster name is Fluffy
+	 * has 2 antenna
+	 * has 4 legs
+	 * has 0 nose
+	 * has 2 eyes
+	 * does not have a belly button 
+	 */
+
+
+
+
+
+
+
+
+}
+
